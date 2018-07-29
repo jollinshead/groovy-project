f7176a47d3e193622cb3e636052a26334c9390ba
Joseph Hollinshead <jhollinshead@hotels.com>
2018-07-29 18:11:16 +0100

    Rename project

--- src/main/groovy/com/example/groovyproject/GroovyProjectApplication.groovy
+++ src/main/groovy/com/example/groovyproject/GroovyProjectApplication.groovy
@@ -1,12 +1,12 @@
-package com.example.demo
+package com.example.groovyproject
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class DemoApplication {
+class GroovyProjectApplication {
 
 	static void main(String[] args) {
-		SpringApplication.run DemoApplication, args
+		SpringApplication.run GroovyProjectApplication, args
 	}
 }
