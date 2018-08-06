91208b21d6a4f169928634890b4eb3cfaf106635
Joseph Hollinshead <jhollinshead@hotels.com>
2018-08-06 08:22:12 +0100

    Playing around...

--- src/main/groovy/com/example/groovyproject/GroovyProjectApplication.groovy
+++ src/main/groovy/com/example/groovyproject/GroovyProjectApplication.groovy
@@ -8,5 +8,16 @@ class GroovyProjectApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run GroovyProjectApplication, args
+		process()
+	}
+
+	static void process() {
+		println("What's your age?")
+
+		def age = 33
+
+		def test = 10.0G, test2 = -1.0G
+		println("test: ${test**test2}, Class: ${test.getClass()}" +
+				"\nAge: $age, Class: ${age.getClass()}" * 2)
 	}
 }
